18e247ddebd0b6e608b3553c3c3b8285ba4f808a
acol <personal@devcol.org>
2026-03-25 17:08:00 +0200

    :)

--- Ask3/vhdl/bench/fir_bench.vhd
+++ Ask3/vhdl/bench/fir_bench.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity TbFirFilter is
 end entity;
@@ -30,6 +31,170 @@ begin
     Reset <= '1';
     wait for CLK_PERIOD * 2;
     Reset <= '0';
+    
+    
+    ValidIn <= '1';
+    X<= std_logic_vector(to_unsigned(208 ,8));
+    wait for CLK_PERIOD;
+    X <= "00000000";
+    ValidIn <= '0';
+    wait for CLK_PERIOD * 7;
+   
+    wait for CLK_PERIOD * 5;
+   
+    ValidIn <= '1';
+    X<= std_logic_vector(to_unsigned(231 ,8));
+    wait for CLK_PERIOD;
+    X <= "00000000";
+    ValidIn <= '0';
+    wait for CLK_PERIOD * 7;
+    
+    ValidIn <= '1';
+    X<= std_logic_vector(to_unsigned(32 ,8));
+    wait for CLK_PERIOD;
+    X <= "00000000";
+    ValidIn <= '0';
+    wait for CLK_PERIOD * 7;
+    
+    ValidIn <= '1';
+    X<= std_logic_vector(to_unsigned(233 ,8));
+    wait for CLK_PERIOD;
+    X <= "00000000";
+    ValidIn <= '0';
+    wait for CLK_PERIOD * 7;
+
+    ValidIn <= '1';
+    X<= std_logic_vector(to_unsigned(161 ,8));
+    wait for CLK_PERIOD;
+    X <= "00000000";
+    ValidIn <= '0';
+    wait for CLK_PERIOD * 7;
+
+    ValidIn <= '1';
+    X<= std_logic_vector(to_unsigned(24 ,8));
+    wait for CLK_PERIOD;
+    X <= "00000000";
+    ValidIn <= '0';
+    wait for CLK_PERIOD * 7;
+    
+        ValidIn <= '1';
+    X<= std_logic_vector(to_unsigned(71 ,8));
+    wait for CLK_PERIOD;
+    X <= "00000000";
+    ValidIn <= '0';
+    wait for CLK_PERIOD * 7;
+
+    ValidIn <= '1';
+    X<= std_logic_vector(to_unsigned(140 ,8));
+    wait for CLK_PERIOD;
+    X <= "00000000";
+    ValidIn <= '0';
+    wait for CLK_PERIOD * 7;
+
+    ValidIn <= '1';
+    X<= std_logic_vector(to_unsigned(245 ,8));
+    wait for CLK_PERIOD;
+    X <= "00000000";
+    ValidIn <= '0';
+    wait for CLK_PERIOD * 7;
+
+    ValidIn <= '1';
+    X<= std_logic_vector(to_unsigned(247 ,8));
+    wait for CLK_PERIOD;
+    X <= "00000000";
+    ValidIn <= '0';
+    wait for CLK_PERIOD * 7;
+
+    ValidIn <= '1';
+    X<= std_logic_vector(to_unsigned(40 ,8));
+    wait for CLK_PERIOD;
+    X <= "00000000";
+    ValidIn <= '0';
+    wait for CLK_PERIOD * 7;
+    
+    ValidIn <= '1';
+    X<= std_logic_vector(to_unsigned(248 ,8));
+    wait for CLK_PERIOD;
+    X <= "00000000";
+    ValidIn <= '0';
+    wait for CLK_PERIOD * 7;
+    
+    ValidIn <= '1';
+    X<= std_logic_vector(to_unsigned(245 ,8));
+    wait for CLK_PERIOD;
+    X <= "00000000";
+    ValidIn <= '0';
+    wait for CLK_PERIOD * 7;
+    
+    ValidIn <= '1';
+    X<= std_logic_vector(to_unsigned(124 ,8));
+    wait for CLK_PERIOD;
+    X <= "00000000";
+    ValidIn <= '0';
+    wait for CLK_PERIOD * 7;    
+        
+    ValidIn <= '1';
+    X<= std_logic_vector(to_unsigned(204 ,8));
+    wait for CLK_PERIOD;
+    X <= "00000000";
+    ValidIn <= '0';
+    wait for CLK_PERIOD * 7;
+        
+    ValidIn <= '1';
+    X<= std_logic_vector(to_unsigned(36 ,8));
+    wait for CLK_PERIOD;
+    X <= "00000000";
+    ValidIn <= '0';
+    wait for CLK_PERIOD * 7;
+    
+    ValidIn <= '1';
+    X<= std_logic_vector(to_unsigned(107 ,8));
+    wait for CLK_PERIOD;
+    X <= "00000000";
+    ValidIn <= '0';
+    wait for CLK_PERIOD * 7;
+    
+        
+    ValidIn <= '1';
+    X<= std_logic_vector(to_unsigned(234 ,8));
+    wait for CLK_PERIOD;
+    X <= "00000000";
+    ValidIn <= '0';
+    wait for CLK_PERIOD * 7;
+    
+        
+    ValidIn <= '1';
+    X<= std_logic_vector(to_unsigned(202 ,8));
+    wait for CLK_PERIOD;
+    X <= "00000000";
+    ValidIn <= '0';
+    wait for CLK_PERIOD * 7;
+    
+        
+    ValidIn <= '1';
+    X<= std_logic_vector(to_unsigned(245 ,8));
+    wait for CLK_PERIOD;
+    X <= "00000000";
+    ValidIn <= '0';
+    wait for CLK_PERIOD * 7;
+    
+    -- flush
+    for i in 0 to 9 loop
+        ValidIn <= '1';
+        X<= "00000000";
+        wait for CLK_PERIOD;
+        ValidIn <= '0';
+        wait for CLK_PERIOD * 7; 
+    end loop;
+    
+    
+    
+    
+--------------------------------------------------------------------------- 
+    
+    
+    
+    Reset<= '0';
     -- Dirac
     for i in 0 to 9 loop
         ValidIn <= '1';
@@ -56,6 +221,7 @@ begin
         wait for CLK_PERIOD * 7; 
     end loop;
     -- flush
+    
     wait for CLK_PERIOD * 20;
     
     
