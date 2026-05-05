552d97ba9935407873dd46f4751a4b7423b17faf
acol <personal@devcol.org>
2026-05-05 14:06:46 +0300

    :)

--- Ask5/c/helloworld.c
+++ Ask5/c/helloworld.c
@@ -7,12 +7,12 @@
 #include "xtime_l.h"
 
 #define TX_DMA_ID                 XPAR_PS2PL_DMA_DEVICE_ID
-#define TX_DMA_MM2S_LENGTH_ADDR  (XPAR_PS2PL_DMA_BASEADDR + 0x28) 
+#define TX_DMA_MM2S_LENGTH_ADDR  (XPAR_PS2PL_DMA_BASEADDR + 0x28)
 #define RX_DMA_ID                 XPAR_PL2PS_DMA_DEVICE_ID
-#define RX_DMA_S2MM_LENGTH_ADDR  (XPAR_PL2PS_DMA_BASEADDR + 0x58) 
+#define RX_DMA_S2MM_LENGTH_ADDR  (XPAR_PL2PS_DMA_BASEADDR + 0x58)
 
-#define TX_BUFFER (XPAR_DDR_MEM_BASEADDR + 0x08000000) 
-#define RX_BUFFER (XPAR_DDR_MEM_BASEADDR + 0x10000000) 
+#define TX_BUFFER (XPAR_DDR_MEM_BASEADDR + 0x08000000)
+#define RX_BUFFER (XPAR_DDR_MEM_BASEADDR + 0x10000000)
 
 #define IMAGE_WIDTH  1024
 #define IMAGE_HEIGHT 1024
@@ -24,7 +24,7 @@ XAxiDma TxAxiDma;
 XAxiDma RxAxiDma;
 
 typedef unsigned char u8;
-typedef unsigned int  u32;
+//typedef unsigned int  u32;
 typedef int           i32;
 typedef float         f32;
 typedef double        f64;
@@ -36,25 +36,25 @@ static inline u8 GetPixel(u8* Buffer, i32 Row, i32 Col, i32 Width, i32 Height)
   return Buffer[Row * Width + Col];
 }
 
-void Debayer(u8* InBuffer, u32* OutBuffer, i32 Width, i32 Height) 
+void Debayer(u8* InBuffer, u32* OutBuffer, i32 Width, i32 Height)
 {
-  for(i32 Row = 0; Row < Height; Row++) 
+  for(i32 Row = 0; Row < Height; Row++)
   {
-    for(i32 Col = 0; Col < Width; Col++) 
+    for(i32 Col = 0; Col < Width; Col++)
     {
       u8 Center = GetPixel(InBuffer, Row, Col, Width, Height);
-      
-      u32 SumVert = GetPixel(InBuffer, Row - 1, Col, Width, Height) + 
+
+      u32 SumVert = GetPixel(InBuffer, Row - 1, Col, Width, Height) +
                     GetPixel(InBuffer, Row + 1, Col, Width, Height);
-                    
-      u32 SumHorz = GetPixel(InBuffer, Row, Col - 1, Width, Height) + 
+
+      u32 SumHorz = GetPixel(InBuffer, Row, Col - 1, Width, Height) +
                     GetPixel(InBuffer, Row, Col + 1, Width, Height);
-                    
+
       u32 SumCross = SumVert + SumHorz;
-      
-      u32 SumDiag = GetPixel(InBuffer, Row - 1, Col - 1, Width, Height) + 
+
+      u32 SumDiag = GetPixel(InBuffer, Row - 1, Col - 1, Width, Height) +
                     GetPixel(InBuffer, Row - 1, Col + 1, Width, Height) +
-                    GetPixel(InBuffer, Row + 1, Col - 1, Width, Height) + 
+                    GetPixel(InBuffer, Row + 1, Col - 1, Width, Height) +
                     GetPixel(InBuffer, Row + 1, Col + 1, Width, Height);
 
       u8 PixelR, PixelG, PixelB;
@@ -64,19 +64,19 @@ void Debayer(u8* InBuffer, u32* OutBuffer, i32 Width, i32 Height)
         PixelR = (u8)(SumVert >> 1);
         PixelG = Center;
         PixelB = (u8)(SumHorz >> 1);
-      } 
+      }
       else if ((Row % 2 == 0) && (Col % 2 != 0)) {
         // GB row B col
         PixelR = (u8)(SumDiag >> 2);
         PixelG = (u8)(SumCross >> 2);
         PixelB = Center;
-      } 
+      }
       else if ((Row % 2 != 0) && (Col % 2 == 0)) {
         // RG row R col
         PixelR = Center;
         PixelG = (u8)(SumCross >> 2);
         PixelB = (u8)(SumDiag >> 2);
-      } 
+      }
       else {
         // RG row G col
         PixelR = (u8)(SumHorz >> 1);
@@ -98,14 +98,14 @@ i32 main()
     XTime PostExecCyclesFpga = 0;
     XTime PreExecCyclesSw = 0;
     XTime PostExecCyclesSw = 0;
-    
+
     XAxiDma_Config *TxConfig;
     XAxiDma_Config *RxConfig;
     i32 Status;
 
     u8 *TxBuffer = (u8*)TX_BUFFER;
     u32 *RxBuffer = (u32*)RX_BUFFER;
-    u32 *SwBuffer = (u32*)(RX_BUFFER + RX_BYTES); 
+    u32 *SwBuffer = (u32*)(RX_BUFFER + RX_BYTES);
 
     print("HELLO 1\r\n");
 
@@ -122,10 +122,14 @@ i32 main()
     Status = XAxiDma_CfgInitialize(&RxAxiDma, RxConfig);
     if (Status != XST_SUCCESS) return XST_FAILURE;
 
-    XAxiDma_IntrDisable(&dma1, XAXIDMA_IRQ_ALL_MASK, XAXIDMA_DEVICE_TO_DMA);
-    XAxiDma_IntrDisable(&dma1, XAXIDMA_IRQ_ALL_MASK, XAXIDMA_DMA_TO_DEVICE);
+    XAxiDma_IntrDisable(&TxAxiDma, XAXIDMA_IRQ_ALL_MASK, XAXIDMA_DMA_TO_DEVICE);
+    XAxiDma_IntrDisable(&RxAxiDma, XAXIDMA_IRQ_ALL_MASK, XAXIDMA_DEVICE_TO_DMA);
 
     // TODO(acol): maybe populate TxBuffer with dummy image data?
+    for(i32 Idx = 0; Idx < NUM_PIXELS; Idx++)
+    {
+    	TxBuffer[Idx] = (u8)(Idx %256);
+    }
 
     // NOTE(acol): HW part
     XTime_GetTime(&PreExecCyclesFpga);
@@ -152,7 +156,7 @@ i32 main()
 
     // NOTE(acol): report results
     i32 ErrorCount = 0;
-    for (i32 Idx = 0; Idx < NUM_PIXELS; Idx++) 
+    for (i32 Idx = 0; Idx < NUM_PIXELS; Idx++)
     {
         ErrorCount+= RxBuffer[Idx] != SwBuffer[Idx];
     }
@@ -161,10 +165,10 @@ i32 main()
     i32 SwCycles        = (i32)(PostExecCyclesSw - PreExecCyclesSw);
     f32 Speedup         = (f32)SwCycles / (f32)FpgaCycles;
 
-    print("Total Error: %.2f%%\r\n\r\n", ErrorPercentage);
-    print("HW Exec time: %d cycles\r\n", FpgaCycles);
-    print("SW Exec time: %d cycles\r\n", SwCycles);
-    print("Speedup: %.2f\r\n", Speedup);
+    printf("Total Error: %.2f%%\r\n\r\n", ErrorPercentage);
+    printf("HW Exec time: %d cycles\r\n", FpgaCycles);
+    printf("SW Exec time: %d cycles\r\n", SwCycles);
+    printf("Speedup: %.2f\r\n", Speedup);
 
     cleanup_platform();
     return 0;
